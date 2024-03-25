d08f534080a3340fd7f2b804f9da326e20f3088c
Tehreem <tehreem.sajid@ucalgary.ca>
2024-03-24 18:03:59 -0600

    fixed issues

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Header from './components/Header';
 import HomeMainSection from './components/HomeMainSection';
 import Footer from './components/Footer';
